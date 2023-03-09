7ad868c4feda6d7c370fd39594c486d6d80257db
jiangminbing <jiangminbing@sina.com>
2023-03-09 10:06:05 +0800

    [add]初始化

---
+++ src/main/java/org/example/Demo.java
@@ -0,0 +1,17 @@
+package org.example;
+
+/**
+ * @author jiangmb
+ * @version 1.0.0
+ * @date 2023-03-09 09:56
+ */
+public class Demo {
+    public static void main(String[] args) {
+        // 类型推断
+        var a = 1;
+        var b = 1 + 2.00D;
+        System.out.println(b);
+
+
+    }
+}
